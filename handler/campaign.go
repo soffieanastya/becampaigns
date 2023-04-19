f4a483566c5d528bf675dec4367e88a20f75823e
unknown <soffieanastyaa@gmail.com>
2023-04-19 16:29:38 +0700

    Campaigns formatter

--- handler/campaign.go
+++ handler/campaign.go
@@ -27,12 +27,12 @@ func NewCampaignHandler(service campaign.Service) *campaignHandler {
 
 func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	userID, _ := strconv.Atoi(c.Query("user_id")) // returnya pasti string, jd pake strconv buatkonfersi ke int
-	campaign, err := h.service.GetCampaigns(userID)
+	campaigns, err := h.service.GetCampaigns(userID)
 	if err != nil {
-		response := helper.APIResponse("Error to get campaigns",http.StatusBadRequest,"error",nil)
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
-		return 
+		return
 	}
-	response := helper.APIResponse("List ofcampaigns", http.StatusOK, "success", campaign)
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
